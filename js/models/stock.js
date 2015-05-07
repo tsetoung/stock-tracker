223f920f88fd125a1011d432bd0e31e6ebfd29af
tsetoung <jtmoua@yahoo.com>
2015-05-07 06:43:16 -0400

    mod ajax API

--- js/models/stock.js
+++ js/models/stock.js
@@ -1,12 +1,20 @@
-// define your Stock model here
 function Stock(symbol) {
   this.symbol = symbol;
   this.numShares = 0;
 
-  // your code to search the API and return a Stock object here
+  var stockAPI = "http://dev.markitondemand.com/api/v2/quote/jsonp?symbol=" + symbol
 
-}
+  $.ajax({
+    url: stockAPI,
+    type: "get",
+    dataType: "jsonp"
+  }).done(function(response) {
+    this.companyName = response["Name"];
+    this.price = response["LastPrice"];
+    stockView.render();
+  });
 
 Stock.prototype = {
   totalValue: function() { return this.price * this.numShares; }
+  }
 }
